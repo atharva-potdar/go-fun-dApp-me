2f1473fddb26c1339b64dedbbd9eb3ea3d9864ca
atharva-potdar <atharvapotdar07@gmail.com>
2025-10-11 10:29:31 +0530

    feat: switch to zksync era sepolia testnet

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.30 <0.9.0;
+pragma solidity >=0.8.24 <0.9.0;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
